1c1f77d4ed0600f5ff25077505d90767c7dd3250
gunjan <patelgunjan777@gmail.com>
2016-08-03 08:49:28 +0530

    Bower Files and Date

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express'),
 var app = express();
 
 app.set('port', process.env.PORT || 5555);
+app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
 
